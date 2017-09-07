3a446340a2e14d77a1406f2c3466cf8f72e0281d
Long <wanglong@teco.edu>
2017-09-07 19:52:41 +0200

    code updated, comments added, take use of ofdm_constellation_simulation_speedEstimation.m

--- src/ofdm_constellation_simulation_speedEstimation.m
+++ src/ofdm_constellation_simulation_speedEstimation.m
@@ -1,35 +1,25 @@
-% this file simulated the effect of doppler shift introduced by adobe
-% audition, with different moving speed
-% simulation setting please see the docu in wiz "simulation of OFDM" on
-% 27.07.2017
+
 clear all, close all, clc;
 
-%% -----------generate cplx modulated ofdm signal, proof it ---------------
-
 %% setting
-% by selecting flag_case & speed, you can specify the setting file & 
-% wav file you want to import
 
-
+% if apply moving average filter to v_symbol on each subcarrier
 flag_MAFlt_perSC = 1;
 
-t_duration = 20;  %13s
-load('golaySeq.mat');
+t_duration = 20;  
+%golay sequence
+load('golaySeq.mat');   
 
 N_sc = 256;  %number of carriers
-v = 1;   % 
-if v == 0.1
-    t_duration = 100;
-end
-
-typ = 'l';
 c = 343;  % speed of sound in the air
 
 filename = '../res/exp07-09-2017/setup2-sc256-No1.wav';
 
+% select step_sc accoring to N_sc, step_sc is the distance between active
+% subcarriers 
 switch N_sc
     case 256
-        step_sc = 8;
+        step_sc = 8; 
         golay = G32pair(:,1);
     case 512
         step_sc = 16;
@@ -56,7 +46,7 @@ N_symbol = 50;     % number of symbol in a frame
 N_frame = ceil(t_duration*Fs/(N_symbol*symbolCP_len));
 t = [1:symbolCP_len]'/Fs;
 
-
+% preamble
 f_min = 10000;
 f_max = 14000;
 pre_len = 1024;
@@ -73,7 +63,7 @@ hMod = comm.BPSKModulator;    % creating bpsk modulator system object
 hMod.PhaseOffset = pi/16;     % phase set to pi/16
 
 binary_data = ones(N_sc,1);
-binary_data(sc_active) = golay;
+binary_data(sc_active) = golay; % the binary data transmitted is golay sequence
 BPSK_data = step(hMod,binary_data).*sc_mask;   % this is the bpsk modulated data
 
 len_frame = pre_len + blank_len*2 + symbolCP_len*N_symbol;
@@ -83,26 +73,31 @@ sync_offset = -cp_length/2;
 
 
 %% synchronization
+% new function developed for synchronization
 index_arr = sync_offline(sig_received,preamble,len_frame);
 disp('index_arr');
 disp(index_arr);
+% the diff of index_arr should be around the length of frame, other wise
+% synchronization is wrong
 disp('diff');
 disp(diff(index_arr));
 
+% number of target frames detected by synchronization
 N_targetFrame = length(index_arr);
-N_targetFrame = 20;
+
 %% phase information extraction
-% estimated speed, final version
-v_est_diff = [];
-v_est_grad = [];
-deg_total = [];
-deg_unwraped_total = [];
-deg_unwraped_woSmooth_total = [];
-w_symbol_diff = [];
-w_symbol_gra = [];
+% following variables are all matrix, rows --> symbols, columns --> sc_active
+v_est_diff = []; % speed estimated based on diff(phase)
+v_est_grad = []; % speed estimated based on gradient(phase)
+deg_total = [];  % original phase info 
+deg_unwraped_total = []; % unwraped phase info (unwraped in frame, not between frames)
+deg_unwraped_woSmooth_total = []; % without sooth
+w_symbol_diff = []; % \omiga per symbol based on diff
+w_symbol_gra = []; %\omiga per symbol based on gradient
 
 % loop through multiple frames
-for i_frame = 1:N_targetFrame
+for i_frame = 1:N_targetFrameS
+    % create a matrix to record the phase info in a frame
     deg = zeros(N_symbol,length(sc_active));
     
     %loop through OFDM symbols
@@ -110,11 +105,7 @@ for i_frame = 1:N_targetFrame
         index_symbol = j;
         i_start = index_arr(i_frame) + blank_len + symbolCP_len*(index_symbol-1) + 1;
         i_end = i_start + symbolCP_len - 1;
-    %     v_end = relative_speed(v,dis,i_end);
-    %     v_start = relative_speed(v,dis,i_start);
-    %     disp(['relative speed of segment #',num2str(j),' is ',num2str(v_start),'-',num2str(v_end)]);
         target_sym = sig_received(i_start: i_end);
-
         symbol_woCP = target_sym(cp_length + 1 + sync_offset: end + sync_offset);
 
         % map data to complex plane, constellation
@@ -122,13 +113,8 @@ for i_frame = 1:N_targetFrame
         BPSK_demodulated = frequency_data(2:N_sc+1);
         BPSK_demodulated = BPSK_demodulated / max(abs(BPSK_demodulated(sc_active)));
 
-    %     figure;
-    %     hold on;
-    %     plot(linspace(0,Fs/2,length(BPSK_data)),abs(BPSK_data));
-    %     plot(linspace(0,Fs/2,length(BPSK_demodulated)),abs(BPSK_demodulated));
-    %     legend('original','received');
-
         deg(j,:) = angle(BPSK_demodulated(sc_active));
+        % this segment is for debug, to inspect constellation symbol by symbol
         if j>40 && 0 && i_frame ==1
             figure;
             xlim([-1.2 1.2]);
@@ -150,22 +136,23 @@ for i_frame = 1:N_targetFrame
     deg_total = [deg_total;deg];
     deg_unwraped_woSmooth = unwrap(deg);
     deg_unwraped_woSmooth_total = [deg_unwraped_woSmooth_total;deg_unwraped_woSmooth];
+    % options, if smooth the unwarped phase
     for k = 1:length(sc_active)
         deg_unwraped(:,k) = smooth(unwrap(deg(:,k)),'moving');
     end
-%     deg_unwraped = smooth(unwrap(deg),'rlowess');
     deg_unwraped_total = [deg_unwraped_total;deg_unwraped];
     
-    % angular speed based on phase diff
+    % angular speed based on phase diff(unit: pi)
     diff_temp = diff(deg_unwraped/pi);
     w_symbol_diff = [w_symbol_diff;diff_temp(1,:);diff_temp];
     
-    % angular speed based on phase gradient
+    % angular speed based on phase gradient(unit: pi)
     [f_temp,w_symbol_temp] = gradient(deg_unwraped/pi);
     w_symbol_gra = [w_symbol_gra;w_symbol_temp];
    
 end
 
+% this is for debug, see the effect of smooth on the phase
 for k = 1:length(sc_active)
     figure;
     hold on;
@@ -176,7 +163,6 @@ end
 
 %% speed estimation based on phase information
 factor = 2.85;
-% factor = 2;
 % symbol level speed for the whole sequence 
 v_symbol_diff = bsxfun(@rdivide, w_symbol_diff.*c./factor, sc_active);
 v_symbol_gra = bsxfun(@rdivide, w_symbol_gra.*c./factor, sc_active);
@@ -198,23 +184,26 @@ else
 end
 
 % select subcarriers which provide most smoothy curves
-% use the variation of gradient of the curves
+% according to the variation of gradient of the curves
 [fx_diff,fy_diff] = gradient(v_symbol_diff_step2);
 [fx_gra,fy_gra] = gradient(v_symbol_gra_step2);
-% select 10 subcarriers with least peaks to calculate the avg 
+
+
+% select N_sc_avg subcarriers with least peaks to calculate the avg 
+N_sc_avg = 5;
 [temp1, ix_diff] = sort(var(fy_diff));
 [temp2, ix_gra] = sort(var(fy_gra));
 
-% averaged symbol speed estimation across 10 subcarriers
-N_sc_avg = 5;
+% averaged symbol speed estimation across N_sc_avg subcarriers
 v_symbol_diff_avg = mean(v_symbol_diff_step2(:,ix_diff(1:N_sc_avg)),2);
 v_symbol_gra_avg = mean(v_symbol_gra_step2(:,ix_gra(1:N_sc_avg)),2);
-% use the subcarrier with least fluctuation
+% pick out the subcarrier with least fluctuation
 v_symbol_diff_min = v_symbol_diff_step2(:,ix_diff(1));
 v_symbol_gra_min = v_symbol_gra_step2(:,ix_gra(1));
 
 dt = [1:length(v_symbol_diff_avg)]*symbolCP_len/Fs;
 
+% show symbol speed estimation across all the target frames 
 figure;
 hold;
 % plot(dt,v_symbol_diff_avg);
@@ -225,6 +214,8 @@ plot(dt,v_symbol_gra_min);
 legend('diff\_min','gra\_min');
 
 
+% transform symbol speed to sample speed, to compare with groud truth, not
+% usefull right now since we do not have a ground truth
 flag_symbol2sample = 0 ;
 if flag_symbol2sample
     % transform speed est from symbol level to sample level
@@ -312,11 +303,6 @@ end
 flag_perSC = 1;
 
 if flag_perSC
-    lag_movavg = 8;
-    a = 1;
-    b = ones(1,lag_movavg)/lag_movavg;
-
-    flag_MaFlt_singleChannel = 1;
 
     for k = 1:length(sc_active)
         sc_index = sc_active(k);
@@ -345,15 +331,7 @@ if flag_perSC
         ylabel('\omega /pi');
         title('angular speed');
 
-        if flag_MaFlt_singleChannel
-            v_symbol_diff_step2 = filter(b,a,v_symbol_diff(:,k));
-            v_symbol_gra_step2 = filter(b,a,v_symbol_gra(:,k));
-        else
-            v_symbol_diff_step2 = v_symbol_diff(:,k);
-            v_symbol_gra_step2 = v_symbol_gra(:,k);
-        end
-
-        % plot ground truth speed and estimated speed
+        % plot estimated speed per symbol
         subplot(224);
         hold on;
         plot(v_symbol_gra(:,k));
